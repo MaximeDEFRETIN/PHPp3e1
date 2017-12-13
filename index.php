dff92b043c55cd5eb56c2fc0d5def37ef61a6f2d
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-13 10:04:25 +0100

    exercice 1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie3 : Exercice 1</title>
+  </head>
+  <body>
+    <?php
+     $number = 0;
+
+     while ($number <= 10) {
+       echo $number . "\r";
+
+       $number++;
+     }
+    ?>
+  </body>
+</html>
